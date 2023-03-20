2b750417704861d2c67434a7936e37e4dd8513d2
Tahir Rafique <121024103+tahir-rafique@users.noreply.github.com>
2023-03-20 21:46:48 +0500

    made some

---
+++ main.js
@@ -0,0 +1,16 @@
+const display = document.querySelector("#display");
+const button = decument.querySelectorAll("button");
+
+button.forEach((btn) => {
+    btn.addEventListener("click", () => {
+     if(btn.id === "=") {
+       display.value = eval(display.value);
+    } else if (btn.id === "ac") {
+        display.value = "";
+    } else if (btn.id == "de") {
+        display.value = display.value.slice(0, -1);
+    } else{
+        display.value += btn.id;
+    }
+ });
+});
